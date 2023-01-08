f741469946cb52243ba4f11badd8c885cbc8cd61
DivyaLabhane <divyalabhane@gmail.com>
2023-01-08 15:05:49 +0530

    Code is added

---
+++ src/com/velocity/ArrayListDemo.java
@@ -0,0 +1,16 @@
+package com.velocity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListDemo {
+	
+	public static void main(String[] args) {
+		List<String> list = new ArrayList<String>();
+		list.add("Ram");
+		list.add("Shyam");
+		
+		System.out.println(list);
+	}
+
+}
